84e8554f50e7ee848e1b60f31027c7dd0a10ac00
Marck Vaisman <marck@vaisman.us>
2020-08-25 10:45:11 -0400

    update syllabus, add directory structure, knit multiple outputs

--- schedule.R
+++ schedule.R
@@ -15,21 +15,21 @@ lec_sched_vec <- c(
   sort(.)
 
 course_tbl <- tribble(
-  ~topics, ~activity, ~readings, ~due,
-  "History and purpose of dataviz, designing for an Audience", "Setup environment", "", "",
-  "Picking the right visualization", "Build an R Markdown website", "*", "A1 due Fri 9/4",
-  "Making readable graphics, putting it all together", "Activity 3", "*", "A2 due Fri 9/11",
-  "Tools overview", "Activity 4", "*", "A3 due Fri 9/18",
-  "Data prep for visualization", "Wrangle/structure a complex dataset for dataviz", "*", "",
-  "EDA Visualization, naniar, outliers", "Understand your data", "*", "A4 due Fri 10/2",
-  "Static graphics with R - ggplot and ecosystem", "Make graphs with R", "*", "A5 due Fri 10/9",
-  "Static graphics with Python - matplotlib", "Make graphs with Python", "*", "A6 due Fri 10/16",
-  "Maps and geospatial data", "Cloropleth: mapping the FL 2000 election", "*", "A7 due Fri 10/23",
-  "Networks", "Build and draw a network", "*", "A8 due Fri 10/31",
-  "Dynamic graphs 1", "Activity 11", "*", "A9 due Fri 11/6",
-  "Dynamic graphs 2", "Activity 12", "*", "",
-  "Visualizing model dianostics and results", "ML diagnostics", "*", "A10 due Fri 11/20",
-  "Wrapup", "", "", "Final Project and Online Portfolio due Fri 12/11",
+  ~module, ~topics, ~activity, ~readings, ~due,
+  "1 - Conceptual", "History and purpose of dataviz, designing for an Audience", "Setup environment", "", "",
+  "1 - Conceptual", "Picking the right visualization", "Build an R Markdown website", "*", "A1 due Fri 9/4",
+  "1 - Conceptual", "Making readable graphics, putting it all together", "Activity 3", "*", "A2 due Fri 9/11",
+  "2 - Tools & Data", "Tools overview", "Activity 4", "*", "A3 due Fri 9/18",
+  "2 - Tools & Data", "Data prep for visualization", "Wrangle/structure a complex dataset for dataviz", "*", "",
+  "2 - Tools & Data", "EDA Visualization, naniar, outliers", "Understand your data", "*", "A4 due Fri 10/2",
+  "3 - Static Visuals", "Static graphics with R - ggplot and ecosystem", "Make graphs with R", "*", "A5 due Fri 10/9",
+  "3 - Static Visuals", "Static graphics with Python - matplotlib", "Make graphs with Python", "*", "A6 due Fri 10/16",
+  "4 - Specialized Visuals", "Maps and geospatial data", "Cloropleth: mapping the FL 2000 election", "*", "A7 due Fri 10/23",
+  "4 - Specialized Visuals", "Networks", "Build and draw a network", "*", "A8 due Fri 10/31",
+  "5 - Dynamic Visuals", "Dynamic graphs 1", "Activity 11", "*", "A9 due Fri 11/6",
+  "5 - Dynamic Visuals", "Dynamic graphs 2", "Activity 12", "*", "",
+  "6 - ML", "Visualizing model dianostics and results", "ML diagnostics", "*", "A10 due Fri 11/20",
+  "", "Wrapup", "", "", "Final Project and Online Portfolio due Fri 12/11",
 )
 
 
@@ -41,7 +41,7 @@ sched <-
 sched <-
   sched %>%
   mutate(date = format(date, "%b %d")) %>%
-  select(session, date, topics, activity, readings, due)
+  select(session, module, date, topics, activity, readings, due)
 
 # sched %>%
 #   knitr:::kable(., "markdown",
