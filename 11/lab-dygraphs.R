a50786d414730ab49a82d3073dd07b6f8618cd88
Marck Vaisman <marck@vaisman.us>
2020-11-12 19:10:58 -0500

    snapshot 2020-11-12 updated lab

--- 11/lab-dygraphs.R
+++ 11/lab-dygraphs.R
@@ -1,62 +1,73 @@
-install.packages("dygraphs")
+# dygraph examples
+
 library(dygraphs)
 
-value<-sample(100:800,100,replace = T)
-time<-seq(from=Sys.time(),to=Sys.time()+60*60*24*99,by="day")
-df<-data.frame(time=time,value=value)
+value <- sample(100:800, 100, replace = T)
+time <- seq(from = Sys.time(),
+            to = Sys.time() + 60 * 60 * 24 * 99,
+            by = "day")
+df <- data.frame(time = time, value = value)
+df
 
-Data should be converted to extensible time-series type. The data frame can not be used directly with dygraph function.
+# Data should be converted to extensible time-series type.
+# The data frame can not be used directly with dygraph function.
 
 dygraph(df)
-# Error in dygraph(df) : Unsupported type passed to argument 'data'.
 
-  Thus, we need to convert our data frame into xts type object. A zoo and xts libraries help us to convert data frame. You may need to install them if they are not available in your machine.
+# Error in dygraph(df) : Unsupported type passed to argument 'data'.
+# Thus, we need to convert our data frame into xts type object. 
+# A zoo and xts libraries help us to convert data frame. 
+# You may need to install them if they are not available in your machine.
 
 library(zoo)
 library(xts)
 
- Converting df_data to xts type as below.
+# Convert df_data to xts type as below.
+dy_data <- df %>% read.zoo() %>% as.xts()
 
-dy_data<-df %>% read.zoo() %>% as.xts()
-
-Now, we can plot dy_data with dygraph function.
+# Now, we can plot dy_data with dygraph function.
 
 dygraph(dy_data)
 
-dygraph(dy_data) %>% 
-  dyOptions(colors="red", pointSize = 2,drawGrid =T,stepPlot = T) %>%
+# Add some options to the dygraph
+dygraph(dy_data) %>%
+  dyOptions(
+    colors = "red",
+    pointSize = 2,
+    drawGrid = T,
+    stepPlot = T
+  ) %>%
   dyRangeSelector()
 
 
-# https://bookdown.org/kochiuyu/Technical-Analysis-with-R/dygraphs-package.html
+## Using quantmod and dygraph for plotting different financial time-series
 
-
-install.packages("quantmod")
 library(quantmod)
+
 # getSymbols(): to get data
 getSymbols("AAPL")
+head(AAPL, n = 3)
 
-head(AAPL,n=3)
+price <- OHLC(AAPL) # Open Hi Lo Close for ticker
+head(price, n = 3)
 
-price<-OHLC(AAPL)
-head(price, n=3)
-
-library(dygraphs)
-getSymbols("AAPL")
 getSymbols("SPY")
 
 dygraph(OHLC(AAPL))
 
-graph<-dygraph(Cl(SPY), main = "SPY") 
-dyShading(graph, from="2007-08-09", 
-          to="2011-05-11", color="#FFE6E6")
+graph <- dygraph(Cl(SPY), main = "SPY")
 
-graph<-dygraph(OHLC(AAPL), main = "AAPL") 
-graph<-dyEvent(graph,"2007-6-29",
-               "iphone", labelLoc = "bottom") 
-graph<-dyEvent(graph,"2010-5-6", 
-               "Flash Crash", labelLoc = "bottom") 
-graph<-dyEvent(graph,"2014-6-6", 
-               "Split", labelLoc = "bottom") 
-dyEvent(graph,"2011-10-5",
-        "Jobs", labelLoc = "bottom") 
+dyShading(graph,
+          from = "2007-08-09",
+          to = "2011-05-11",
+          color = "#FFE6E6")
+
+graph <- dygraph(OHLC(AAPL), main = "AAPL")
+graph <- dyEvent(graph, "2007-6-29",
+                 "iphone", labelLoc = "bottom")
+graph <- dyEvent(graph, "2010-5-6",
+                 "Flash Crash", labelLoc = "bottom")
+graph <- dyEvent(graph, "2014-6-6",
+                 "Split", labelLoc = "bottom")
+dyEvent(graph, "2011-10-5",
+        "Jobs", labelLoc = "bottom")
--- 11/lab-leaflet.R
+++ 11/lab-leaflet.R
@@ -3,11 +3,11 @@
 
 library(dplyr)
 library(ggplot2)
-library(rjson)
+#library(rjson)
 library(jsonlite)
 library(leaflet)
 library(RCurl)
-
+library(jsonlite)
 
 r_birthplace_map <- leaflet() %>%
   addTiles() %>%  # use the default base map which is OpenStreetMap tiles
@@ -20,10 +20,82 @@ full_url <- paste0(base_url, "station_status=Active",
             "&county=BOULDER")
 water_data <- getURL(URLencode(full_url))
 
-water_data_df <- rjson::fromJSON(water_data) %>%
-  flatten(recursive = TRUE) # remove the nested data frame
+water_data_df <- jsonlite::fromJSON(water_data, flatten = TRUE) 
 
 # turn columns to numeric and remove NA values
 water_data_df <- water_data_df %>%
-  mutate_at(vars(amount, location.latitude, location.longitude), funs(as.numeric)) %>%
+  mutate(across(c(amount, location.latitude, location.longitude), as.numeric)) %>% 
   filter(!is.na(location.latitude))
+
+# create leaflet map
+water_locations_map <- leaflet(water_data_df) %>%
+  addTiles() %>%
+  addCircleMarkers(lng = ~location.longitude,
+                   lat = ~location.latitude)
+
+# Same code as above but without using pipes (we like pipes better)
+water_locations_map <- leaflet(water_data_df)
+water_locations_map <- addTiles(water_locations_map)
+water_locations_map <- addCircleMarkers(water_locations_map, lng = ~location.longitude,
+                        lat = ~location.latitude)
+water_locations_map
+
+
+# customize leaflet map
+leaflet(water_data_df) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addMarkers(lng = ~location.longitude, lat = ~location.latitude,
+             popup = ~station_name)
+
+# Specify custom icon
+url <- "http://tinyurl.com/jeybtwj"
+water <- makeIcon(url, url, 24, 24)
+
+map <- leaflet(water_data_df) %>%
+  addProviderTiles("Stamen.Terrain") %>%
+  addMarkers(lng = ~location.longitude, lat = ~location.latitude, icon=water,
+             popup = ~paste0(station_name,
+                           "<br/>Discharge: ",
+                           amount))
+
+library(htmlwidgets)
+saveWidget(widget=map,
+           file="water_map3.html",
+           selfcontained=TRUE)
+
+
+pal <- colorFactor(c("navy", "red", "green"),
+                   domain = unique(water_data_df$station_type))
+unique_markers_map_2 <- leaflet(water_data_df) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addCircleMarkers(
+    color = ~pal(station_type),
+    stroke = FALSE, fillOpacity = 0.5,
+    lng = ~location.longitude, lat = ~location.latitude,
+    label = ~as.character(station_type)
+  )
+
+
+water_data_df$station_type <- factor(water_data_df$station_type)
+
+new <- c("red", "green","blue")[water_data_df$station_type]
+
+icons <- awesomeIcons(
+  icon = 'ios-close',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = new
+)
+
+unique_markers_map <- leaflet(water_data_df) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addAwesomeMarkers(lng=~location.longitude, lat=~location.latitude, icon=icons,
+                    popup=~station_name,
+                    label=~as.character(station_name))
+
+unique_markers_map
+
+saveWidget(widget = unique_markers_map,
+           file = "water_map_unique_markers1.html",
+           selfcontained = TRUE)
+
